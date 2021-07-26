c1d31dd2da5edea542aeb10b052eb39fae9a3ae3
alejandra.maqueda <alejandra.maqueda@neoris.com>
2021-07-26 18:38:44 -0500

    Pact auth basic

--- Provider/src/test/java/ContractTest.java
+++ Provider/src/test/java/ContractTest.java
@@ -1,6 +1,7 @@
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.State;
 import au.com.dius.pact.provider.junit.loader.PactBroker;
+import au.com.dius.pact.provider.junit.loader.PactBrokerAuth;
 import au.com.dius.pact.provider.junit.loader.PactFolder;
 import au.com.dius.pact.provider.junit.target.Target;
 import au.com.dius.pact.provider.junit.target.TestTarget;
@@ -9,11 +10,14 @@ import au.com.dius.pact.provider.spring.target.SpringBootHttpTarget;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import producer.UserServiceApplication;
+import au.com.dius.pact.provider.junit.IgnoreNoPactsToVerify;
 
 @RunWith(SpringRestPactRunner.class)
 @Provider("Provider")
 //@PactFolder("src/pacts")
-@PactBroker(host = "localhost")
+@PactBroker(host = "localhost", port="8000",
+authentication =  @PactBrokerAuth(username = "pact_workshop", password = "pact_workshop")
+)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = UserServiceApplication.class)
 public class ContractTest {
 
